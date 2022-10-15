f43015ef1c0a5064cceb61897aa8500e38b28fb6
wyattlake <lakewyatt@gmail.com>
2022-10-15 12:08:19 +0200

    Added comment module

--- pages/forum.tsx
+++ pages/forum.tsx
@@ -3,7 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { CommentSmall } from "../components/CommentSmall";
 import { Navbar } from "../components/Navbar";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Forum.module.css";
+import { useForm } from "react-hook-form";
 
 const Forum: NextPage = () => {
     return (
@@ -14,16 +15,34 @@ const Forum: NextPage = () => {
             </Head>
             <main className={styles.main}>
                 <Navbar style={{ position: "absolute", top: 0 }} />
-                <div>
-                    <h1>Forum</h1>
-                </div>
-                <div className="body">
-                    <CommentSmall
-                        subject="Math"
-                        author="rzcman"
-                        title="Whats the best way to start my essay?"
-                        likes={3}
-                    />
+                <div className={styles.mainSection}>
+                    <div className={styles.pageTitle}>
+                        <h1 className={styles.titleGradient}>Search</h1>
+                        <h1 className={styles.title}>&nbsp;for a question</h1>
+                    </div>
+                    <div className={styles.commentSection}>
+                        <div className={styles.commentList}>
+                            <CommentSmall
+                                subject="Math"
+                                author="rzcman"
+                                title="Whats the best way to start my essay?"
+                                likes={3}
+                            />
+                            <CommentSmall
+                                subject="Math"
+                                author="rzcman"
+                                title="Whats the best way to start my essay?"
+                                likes={3}
+                            />
+                        </div>
+                        <div className={styles.postComment}>
+                            <p className={styles.subtitle}>Pose a Question</p>
+                            <p className={styles.helpText}>
+                                Your question will be answered by the Wonder
+                                community.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
